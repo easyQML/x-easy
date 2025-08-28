fd09539ac12d8c4c8b454f18b47149be041ad364
GooRoo <sergey.olendarenko@gmail.com>
2025-08-28 13:48:16 +0200

    asdf

--- packages/l/lodash.qml/xmake.lua
+++ packages/l/lodash.qml/xmake.lua
@@ -2,7 +2,7 @@ package("lodash.qml")
     set_description("A lodash wrapper for easy use in QML")
 
     add_urls("https://github.com/easyQML/lodash.qml.git")
-    add_versions("4.17", "a86a9c2c8207f208246802dc749eab2af33e8fad")
+    add_versions("4.18", "a86a9c2c8207f208246802dc749eab2af33e8fad")
 
     on_install(function (package)
         local configs = {}

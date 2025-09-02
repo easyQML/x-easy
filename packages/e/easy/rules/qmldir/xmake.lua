55e9a7c1d757f99eb59e54e41015458000769c6b
GooRoo <sergey.olendarenko@gmail.com>
2025-09-02 14:30:48 +0200

    revert(easy): Use subfolders for rules again
    This reverts commit 3059b14a3886e8829bd93dbb1f1102ad60cbe120.

--- packages/e/easy/rules/qmldir/xmake.lua
+++ packages/e/easy/rules/qmldir/xmake.lua
@@ -5,16 +5,16 @@ rule('qmldir')
 
 	on_config(function (target, opt)
 		if target:values('easy.qmldir.multiple_modules') then
-			import('_qmldir').on_config_multiple(target, opt)
+			import('qmldir').on_config_multiple(target, opt)
 		else
-			import('_qmldir').on_config(target, opt)
+			import('qmldir').on_config(target, opt)
 		end
 	end)
 
 	before_build_files(function (target, sourcebatch, opt)
 		if target:values('easy.qmldir.multiple_modules') then
-			import('_qmldir').before_build_files_multiple(target, sourcebatch, opt)
+			import('qmldir').before_build_files_multiple(target, sourcebatch, opt)
 		else
-			import('_qmldir').before_build_files(target, sourcebatch, opt)
+			import('qmldir').before_build_files(target, sourcebatch, opt)
 		end
 	end)
